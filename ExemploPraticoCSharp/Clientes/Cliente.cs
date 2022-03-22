bae8e7d358b47335604e9bd8f30bdc4e3b494112
Lindemberg Rodrigues <bergfla2010@hotmail.com>
2022-03-22 15:58:53 -0300

    implemento das propriedades

---
+++ ExemploPraticoCSharp/Clientes/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using ExemploPraticoCSharp.Pessoas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploPraticoCSharp.Clientes
+{
+    class Cliente : Pessoa
+    {
+        public String  Conta { get; set; }
+        public String Agencia { get; set; }
+        public double Saldo { get; set; }
+        public Cliente(string nome, string CPF, Endereco enderecos, String agencia, String conta) : base(nome, CPF, enderecos)
+        {
+            Conta = conta;
+            Agencia = agencia;
+            Saldo = 0;
+        }
+
+        public string Name { get; set; }
+    }
+}
