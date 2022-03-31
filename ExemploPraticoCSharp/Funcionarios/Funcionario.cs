cf187929ceba1f6e6799f2a03c8faecd65c0a20b
Lindemberg Rodrigues <bergfla2010@hotmail.com>
2022-03-31 12:27:53 -0300

    ajustes

--- ExemploPraticoCSharp/Funcionarios/Funcionario.cs
+++ ExemploPraticoCSharp/Funcionarios/Funcionario.cs
@@ -6,7 +6,7 @@ namespace ExemploPraticoCSharp.Funcionarios
 {
     public class Funcionario : Pessoa
     {
-        public String  Setor { get; set; }
+        public String Setor { get; set; }
         public double Salario { get; }
         public String id { get; }
         public Funcionario(String id, string nome, string CPF, Endereco enderecos, string setor, double salario) : base(nome, CPF, enderecos)
--- ExemploPraticoCSharp/Program.cs
+++ ExemploPraticoCSharp/Program.cs
@@ -6,11 +6,14 @@ namespace ExemploPraticoCSharp
 {
     class Program
     {
-        public static void Main(String [] args)
+        public static void Main(String[] args)
         {
             Menu();
         }
 
+        /// <summary>
+        /// Inicializa o menu de opções do Banco 
+        /// </summary>
         public static void Menu()
         {
             Console.WriteLine("<Primeiro Acesso>\nCadastrando Banco ... ");
@@ -18,40 +21,134 @@ namespace ExemploPraticoCSharp
             Console.WriteLine("\n<Cadastrando colaborador ...");
             Funcionario F = InicialiaColaborador();
             BC.ContrataFuncionario(F);
-            
+
+            Console.WriteLine("(F)uncionário ou (C)olaborador ::");
+            String opcao = Console.ReadLine();
+            if (opcao.ToUpper() == "F")
+            {
+                Funcionario();
+            }
+            else if (opcao.ToUpper() == "C")
+            {
+                Colaborador();
+            }
+            else
+                Console.WriteLine("OPÇÃO INVALIDA!");
+
+
+
         }
-        public static BancoCentral CadastraBanco()
+        /// <summary>
+        /// Menu de opção para manutenção de colaboradores
+        /// </summary>
+        /// <param name="idConta"></param>
+        private static void Colaborador(string idConta)
         {
-            Console.WriteLine("NOME DO BANCO: ");
-            string nome =  Console.ReadLine();
-            Console.WriteLine("ID AGÊNCIA: ");
-            String idAgencia = Console.ReadLine();
-            Console.WriteLine("RUA: ");
-            String rua = Console.ReadLine();
-            Console.WriteLine("BAIRRO: ");
-            String Bairro = Console.ReadLine();
-            Console.WriteLine("CIDADE: ");
-            String cidade = Console.ReadLine();
-            Console.WriteLine("NUMERO: ");
-            int numero = Console.Read();
-            Endereco endereco = new Endereco(rua, Bairro, numero, cidade);
+            Console.WriteLine("(V)erifica Saldo" +
+                 "\n(S)aque" +
+                 "\n(D)epósito" +
+                 "\n(T)ransferir");
+            String op = Console.ReadLine();
+            switch (op.ToUpper())
+            {
+                case "V":
+                    Saldo(idConta);
+                    break;
+                case "S":
+                    Console.WriteLine("Valor para saque: ");
+                    double saque = Console.Read();
+                    saque(idConta,saque);
+                    break;
+                case "D":
+                    Console.WriteLine("Valor para deposito: ");
+                    double deposito = Console.Read();
+                    deposito(idConta,deposito);
+                    break;
+                case "T":
+                    Console.WriteLine("Valor a transferir: ");
+                    double valor = Console.Read();
+                    Console.WriteLine("Agencia: ");
+                    string agencia = Console.ReadLine();
+                    Console.WriteLine("Conta");
+                    double conta = Console.Read();
+                    transferir(idConta, valor, agencia, conta);
 
-
-            return new BancoCentral(nome,idAgencia,endereco);
+                    break;
+            }
         }
 
-        private static BancoCentral InicialiaBanco() 
+        /// <summary>
+        /// Abre o menu de opções do colaborador
+        /// </summary>
+        private static void Funcionario()
         {
-            Endereco endereco = new Endereco("Rua das Flores", "Centro", 152, "Mênfis");
-            
-            return new BancoCentral("MAD", "0211", endereco);
+            Console.WriteLine("(A)brir conta" +
+                "\n(E)ncerrar conta" +
+                "\n(C)ontratar/Cadastrar novo colaborador" +
+                "\n(D)esligar colaborador");
+            string op = Console.ReadLine();
+            switch (op.ToUpper())
+            {
+                case "F":
+                    AbrirConta();
+                    break;
+                case "E":
+                    EncerrarConta();
+                    break;
+                case "C":
+                    CadastraColaborador();
+                    break;
+                case "D":
+                    DesligarColaborador();
+                    break;
+
+            }
         }
 
-        private static Funcionario InicialiaColaborador()
-        {
-            Endereco endereco = new Endereco("Av. Nova América", "Distrito dos Ferros", 365, "Mênfis");
+            /// <summary>
+            /// Abre o menu de cadastro do Banco
+            /// </summary>
+            /// <returns> retorna o Objeto do tipo Banco</returns>
+            private static BancoCentral CadastraBanco()
+            {
+                Console.WriteLine("NOME DO BANCO: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("ID AGÊNCIA: ");
+                String idAgencia = Console.ReadLine();
+                Console.WriteLine("RUA: ");
+                String rua = Console.ReadLine();
+                Console.WriteLine("BAIRRO: ");
+                String Bairro = Console.ReadLine();
+                Console.WriteLine("CIDADE: ");
+                String cidade = Console.ReadLine();
+                Console.WriteLine("NUMERO: ");
+                int numero = Console.Read();
+                Endereco endereco = new Endereco(rua, Bairro, numero, cidade);
 
-            return new Funcionario("8546", "Lindemberg", "123.456.789-09", endereco, "TI", 2750.0);
+
+                return new BancoCentral(nome, idAgencia, endereco);
+            }
+
+            /// <summary>
+            /// Inicializa um banco logo a inicialização do sistema
+            /// </summary>
+            /// <returns>  retorna o Objeto do tipo Banco </returns>
+            private static BancoCentral InicialiaBanco()
+            {
+                Endereco endereco = new Endereco("Rua das Flores", "Centro", 152, "Mênfis");
+
+                return new BancoCentral("MAD", "0211", endereco);
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <returns></returns>
+            private static Funcionario InicialiaColaborador()
+            {
+                Endereco endereco = new Endereco("Av. Nova América", "Distrito dos Ferros", 365, "Mênfis");
+
+                return new Funcionario("8546", "Lindemberg", "123.456.789-09", endereco, "Gerente", 2750.0);
+            }
         }
     }
-}
